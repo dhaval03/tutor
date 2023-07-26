98d4c581ea05443c00ef88f3e8901469311be5a4
AmarRaghuvanshi <arupareliya@animize.net>
2023-07-26 18:23:36 +0530

    add

--- extension/restapi/catalog/controller/rest/login.php
+++ extension/restapi/catalog/controller/rest/login.php
@@ -43,7 +43,7 @@ class Login extends \RestController
 
                     $email = $post['email'];
 
-                    $customer_info = $this->model_account_customer->getCustomerByEmail($email,$telephone);
+                    $customer_info = $this->model_account_customer->getCustomerByEmail($email);
 
                     if ($customer_info && !$customer_info['status']) {
                         $this->json['error'][] = $this->language->get('error_approved');
@@ -150,7 +150,7 @@ class Login extends \RestController
 
                     $email = $post['email'];
 					
-                    $customer_info = $this->model_account_customer->getCustomerByEmailorPhone($email,$telephone);
+                    $customer_info = $this->model_account_customer->getCustomerByEmailorPhone($email);
 
                     if ($customer_info && !$customer_info['status']) {
                         $this->json['error'][] = $this->language->get('error_approved');

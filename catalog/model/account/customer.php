f8d1c46c029e38c0548af78ed127a59cd5989874
AmarRaghuvanshi <arupareliya@animize.net>
2023-07-26 18:21:50 +0530

    updateSession  add

--- catalog/model/account/customer.php
+++ catalog/model/account/customer.php
@@ -181,6 +181,13 @@ class Customer extends \Opencart\System\Engine\Model {
 
 		return $query->row;
 	}
+	
+	public function getCustomerByEmailorPhone($email) {
+		$query = $this->db->query("SELECT * FROM " . DB_PREFIX . "customer WHERE LOWER(email)
+		= '" . $this->db->escape(utf8_strtolower($email)) . "' OR telephone = '" . $this->db->escape(utf8_strtolower($email)) . "'");
+
+		return $query->row;
+	}
 
 	public function getCustomerByCode(string $code): array {
 		$query = $this->db->query("SELECT `customer_id`, `firstname`, `lastname`, `email` FROM `" . DB_PREFIX . "customer` WHERE `code` = '" . $this->db->escape($code) . "' AND `code` != ''");
